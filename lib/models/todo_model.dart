1d51f3e82f97b51e6b9f11d44ea7df5db6722ef2
KYOHEI-jp <kingdomhearts510@gmail.com>
2022-05-19 22:27:11 +0900

    model

--- lib/models/todo_model.dart
+++ lib/models/todo_model.dart
@@ -8,4 +8,14 @@ class Todo {
   final bool completed;
 
   Todo({required this.id, required this.desc, required this.completed});
+
+  List<Object> get props => [id, desc, completed];
+
+  bool get stringify => true;
+}
+
+enum Filter {
+  all,
+  active,
+  completed,
 }
