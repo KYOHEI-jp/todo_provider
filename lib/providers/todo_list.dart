96458fd661d6efd5efc31666c23cb1a7b28c2d7f
KYOHEI-jp <kingdomhearts510@gmail.com>
2022-05-21 00:22:30 +0900

    todolist

--- lib/providers/todo_list.dart
+++ lib/providers/todo_list.dart
@@ -1,5 +1,19 @@
+import '../models/todo_model.dart';
+
 class TodoListState {
   final List<Todo> todos;
 
   TodoListState({required this.todos});
+
+  @override
+  List<Object> get props => [todos];
+
+  @override
+  bool get stringify => true;
+
+  TodoListState copyWith({
+    List<Todo>? todos,
+  }) {
+    return TodoListState(todos: todos ?? this.todos);
+  }
 }
